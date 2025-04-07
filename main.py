ee7e2bf081c13163cf3bd36ce5626fedf8718165
iwansal64 <iwansal64@gmail.com>
2025-04-07 16:48:59 +0700

    Basic code structure

---
+++ main.py
@@ -0,0 +1,46 @@
+import os
+
+#VARIABLES
+
+
+
+#!VARIABLES
+
+
+#SECTION - 1. Preparations
+def preparation():
+	'''
+	Checking requirements, prerequisites, and libraries
+	''' 
+	
+
+#!SECTION
+
+#SECTION - 2. Loading Configurations
+def load_configuration():
+    '''
+	Loading config files
+	'''
+
+#!SECTION
+
+#SECTION - 3. Setting Up Systems and AI
+def setting_up_systems():
+    '''
+	Starting AI, Running up terminals, etc
+	'''
+
+#!SECTION
+
+#SECTION - 4. Main Program
+def main_loop():
+    '''
+	Watching for incoming message, communicate with AI, and controlling chats
+	'''
+
+#!SECTION
+
+preparation()
+load_configuration()
+setting_up_systems()
+main_loop()
